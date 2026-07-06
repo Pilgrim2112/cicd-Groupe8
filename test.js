71c3cf16a49746cf51f730f495dd46d88d7bf447
Benit Batomene <benitbatomene28@gmail.com>
2026-07-06 13:32:18 +0100

    Add tests for GET / endpoint using supertest

---
+++ test.js
@@ -0,0 +1,15 @@
+const request = require('supertest');
+const server = require('./server');
+
+describe('GET /', () => {
+    after((done) => {
+        server.close(done); // Ferme le serveur après le test
+    });
+
+    it('should return status 200 and success message', (done) => {
+        request(server)
+            .get('/')
+            .expect('Content-Type', /json/)
+            .expect(200, { status: "success", message: "Hello Groupe8" }, done);
+    });
+});
